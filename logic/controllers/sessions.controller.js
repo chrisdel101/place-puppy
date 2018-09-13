bdeedc211d46dcb59e5d6e95739284c015dc185e
Chris Del <drumgod101@gmail.com>
2018-09-13 00:35:33 -0400

    add validation to image model. add some stuff to add template

--- logic/controllers/sessions.controller.js
+++ logic/controllers/sessions.controller.js
@@ -1,104 +1,98 @@
-var session = require('express-session')
+const session = require('express-session')
 const User = require("../models/user.model.js")
-var multer  = require('multer')
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, '/uploads')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now() + file.originalname)
-  }
+const multer = require('multer')
+const storage = multer.diskStorage({
+	destination: function(req, file, cb) {
+		cb(null, '/uploads')
+	},
+	filename: function(req, file, cb) {
+		cb(null, file.fieldname + '-' + Date.now() + file.originalname)
+	}
 })
-var upload = multer({ storage: storage})
-var bcrypt = require('bcrypt');
+const upload = multer({storage: storage})
+const bcrypt = require('bcrypt');
 const saltRounds = 10;
 let cookieParser = require('cookie-parser');
 
-
-// var mongoDB = process.env.DB_URI
-// var db = mongoose.connection;
-// db.once('open', function() {
-//   // we're connected!
-// });
-
-// console.log(db.createNewUser())
-
 module.exports = {
-    login: (req, res) => {
-        let username = req.body.username
-        let password = req.body.password
-        // console.log('username', username)
-        // console.log('password', password)
-        User.find({
-            username: String(username)
-        }, 'username',
-        (err, user) => {
-            if(err) console.error(err)
-            // console.log(Array.isArray(user))
-            console.log(`user`, user)
-            let id = user[0].id
-            User.find({
-                _id: id
-            }, (err, arr) => {
-            // if username does not exist
-            if(arr.length <= 0){
-                console.log('username does not exist')
-                flash('info', 'Username does not exist')
-                res.redirect('login')
-            } else {
-                // make sure only one user by name
-                if(arr.length > 1){
-                    console.error('Multiple users detected. Cannnot open')
-                    req.flash('error', 'A error occured')
-                    res.redirect('login')
-                } else {
-                    let obj = arr[0]
-                    console.log(password)
-                    console.log(obj.password)
-                    bcrypt.compare(password, obj.password, function(err, response) {
-                        if(response) {
-                            // console.log('res', response)
-                            // add user to session
-                            req.session.user = obj
-                            // console.log(req.session.user)
-                            console.log('added to session')
-                            req.flash('success', 'Login successfull')
-                            res.redirect('add')
-                        } else {
-                            console.log('incorrect password')
-                            // flash
-                            req.flash('info', 'incorrect password')
-                            res.redirect('login')
-                        }
-                    });
-                }
-            }
-
-        })
-    })
-    },
-    loginDisplay: (req, res) => {
-        res.render('login', {
-            method: 'POST',
-            action:'/login',
-            enctype: 'application/x-www-form-urlencoded',
-            // route name is used in template
-            routeName: req.path,
-            field_one_for: 'username',
-            field_two_for: 'password',
-            field_one_obj: 'usurname',
-            field_two_id: 'password',
-            field_one_type: 'text',
-            field_two_type: 'password',
-            field_one_name: 'username',
-            field_two_name: 'password',
-            fieldOne: 'Username',
-            fieldTwo: 'Password',
-            field_one_placeholder: 'Enter Username',
-            field_two_placeholder: 'Enter Password',
-            button_type: 'submit',
-            button_value: 'submit',
-            buttonField: "Sign In"
-           })
-    }
+	login: (req, res) => {
+		let username = req.body.username
+		let password = req.body.password
+		// console.log('username', username)
+		// console.log('password', password)
+		// Look up username - get ID
+		User.find({
+			username: String(username)
+		}, 'username', (err, userArr) => {
+			if (err)
+				console.error(err)
+				// console.log(Array.isArray(userArr))
+			console.log(`userArr`, userArr)
+			if (!userArr) {
+				console.log('No user. Breaking out of func.')
+				return
+			}
+			if (userArr.length <= 0) {
+				console.log('username does not exist')
+				req.flash('info', 'Username does not exist')
+				res.redirect('login')
+				return
+			}
+			// make sure only one user by name
+			if (userArr.length > 1) {
+				console.error('Multiple users detected. Cannnot open')
+				req.flash('error', 'A error occured')
+				res.redirect('login')
+			}
+			// Use ID to lookup hash for PW
+			let id = userArr[0].id
+			User.find({
+				_id: id
+			}, (err, arr) => {
+				let obj = arr[0]
+				console.log(password)
+				console.log(obj.password)
+				bcrypt.compare(password, obj.password, function(err, response) {
+					if (response) {
+						// console.log('res', response)
+						// add user to session
+						req.session.user = obj
+						// console.log(req.session.user)
+						console.log('added to session')
+						req.flash('success', 'Login successfull')
+						res.redirect('add')
+					} else {
+						console.log('incorrect password')
+						// flash
+						req.flash('info', 'incorrect password')
+						res.redirect('login')
+					}
+				})
+			})
+		})
+	},
+	loginDisplay: (req, res) => {
+		res.render('login', {
+			method: 'POST',
+			action: '/login',
+			enctype: 'application/x-www-form-urlencoded',
+			// route name is used in template
+			routeName: req.path,
+			field_one_for: 'username',
+			field_two_for: 'password',
+			field_one_obj: 'usurname',
+			field_two_id: 'password',
+			field_one_type: 'text',
+			field_two_type: 'password',
+			field_one_name: 'username',
+			field_two_name: 'password',
+			fieldOne: 'Username',
+			fieldTwo: 'Password',
+			field_one_placeholder: 'Enter Username',
+			field_two_placeholder: 'Enter Password',
+			button_type: 'submit',
+			button_value: 'submit',
+			buttonField: "Sign In"
+		})
+	}
 }
--- logic/models/image.model.js
+++ logic/models/image.model.js
@@ -1,16 +1,40 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema
+const assert = require('assert')
 
-  const imageSchema = new mongoose.Schema({
-    filename: String,
-    title: String,
-    photographer:String,
-    description: String,
-    locationTaken:String,
-    tags:Array,
-    data: Buffer,
-    contentType: String
-  });
-    const imageModel = mongoose.model('Image', imageSchema)
+const imageSchema = new mongoose.Schema({
+	filename: String,
+	title: {
+		type: String,
+		required: true
+	},
+	photographer: String,
+	description: String,
+	locationTaken: String,
+	tags: Array,
+	data: {
+		type: Buffer,
+		required: true
+	},
+	contentType: String
+});
+const imageModel = mongoose.model('Image', imageSchema)
 
-    module.exports = imageModel
+module.exports = imageModel
+
+var image = new imageModel();
+console.log(image)
+image.save(function(error) {
+	assert.equal(error.errors['title'].message, 'Path `title` is required.');
+
+	error = image.validateSync();
+	assert.equal(error.errors['title'].message, 'Path `title` is required.');
+});
+
+var image2 = new imageModel({title: 'hello'});
+image2.save(function(error) {
+	assert.equal(error.errors['data'].message, 'Path `data` is required.');
+
+	error = image2.validateSync();
+	assert.equal(error.errors['data'].message, 'Path `data` is required.');
+});
