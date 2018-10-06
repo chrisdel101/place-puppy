bace8ee98b6393b9e660dbea708b5337e65e7f93
Chris Del <drumgod101@gmail.com>
2018-10-06 16:36:06 -0400

    clean up users controller add logs

--- logic/controllers/users.controller.js
+++ logic/controllers/users.controller.js
@@ -1,89 +1,92 @@
-const User = require("../models/user.model.js")
+const User = require('../models/user.model.js')
 const bcrypt = require('bcrypt')
 const saltRounds = 10
-const async = require("async")
+const async = require('async')
 const crypto = require('crypto')
 const nodemailer = require('nodemailer')
+const debug = require('debug')
+const log = debug('users:log')
+const error = debug('users:error')
+
 
 module.exports = {
+    // compare two string
     compareStrs: (pw1, pw2) => {
         return (
             pw1 === pw2
             ? true
             : false)
     },
+    // handle post register request
     register: (req, res) => {
-
         // check that user and pw not blank
         if (req.body.email.length === 0 || req.body.password.length === 0) {
-            console.log('Email or password cannot be blank')
+            error('Email or password cannot be blank')
             req.flash('info', 'Username or password cannot be blank')
             return res.redirect('register')
         }
         // check that both emails match
         // if don't match, kill
         if (!module.exports.compareStrs(req.body.email, req.body['email-confirmation'])) {
-            console.log('emails do not match')
+            error('emails do not match')
             req.flash('info', 'emails do not match')
             return res.redirect('register')
         }
-            console.log('pw1', req.body.password)
-            console.log('pw2', req.body['password-confirmation'])
         // check that both passwords match
         if (!module.exports.compareStrs(req.body.password, req.body['password-confirmation'])) {
-            console.log('passwords do not match')
+            error('passwords do not match')
             req.flash('info', 'passwords do not match')
-            res.redirect('register')
-            return
+            return res.redirect('register')
         }
         if(!module.exports.passwordVerify(req.body.password)){
-            console.log('Password too short. Must be at least 8 characters')
+            error('Password too short. Must be at least 8 characters')
             req.flash('info','Password is too short. Must be at least eight characters.')
             return res.redirect('register')
         }
         // get hash from pw
         bcrypt.hash(req.body.password, saltRounds).then(function(hash) {
-            console.log('hash', hash)
+            log('hash', hash)
             // add user name and hash
             let user = new User({email: req.body.email, password: hash})
-            console.log('user', user)
-
+            log('user', user)
             // lookup to see if username already exists
             User.find({
                 username: String(req.body.email).toLowerCase()
             }, (err, userArr) => {
-                if (err)
-                    console.error('An error in finding occured')
-                console.log('user', user)
+                if (err) {
+                    error('An error in finding occured.')
+                    res.flash('error', 'An database error occured. Try again.')
+                    return res.redirect('register')
+            }
+                log('user', user)
                 // if user exists
                 if (userArr.length > 0) {
-                    console.log('That email already exists. Register with another')
+                    log('That email already exists. Register with another')
                     req.flash('info', 'That email already exists. Register with another.')
                     // redirect to same page
-                    res.redirect('register')
+                    return res.redirect('register')
                 } else {
                     // save user
                     let promise = user.save()
                     promise.then(userData => {
-                        console.log('user saved')
+                        log('user saved')
                         req.flash('success', 'User successfully saved.')
-                        res.redirect('register')
+                        return res.redirect('register')
                     }).catch(err => {
-                        console.log('an error occured', err)
+                        error('an error occured', err)
                         req.flash('error', `An Error occurred in saving: ${err}`)
-                        res.redirect('register')
+                        return res.redirect('register')
                     })
                 }
             })
-            return
         }).catch(err => {
-            console.log("There was an err in hashing", err)
+            error('There was an err in hashing', err)
         })
 
     },
+    // render register template
     registerDisplay: (req, res) => {
-        console.log('register Displayfired')
-        res.render('register', {
+        return res.render('register', {
             method: 'POST',
             action: '/register',
             enctype: 'application/x-www-form-urlencoded',
@@ -117,11 +120,13 @@ module.exports = {
             buttonField: 'Register'
         })
     },
+    // send user reset email
     forgotPassword: (req, res, next) => {
         async.waterfall([
             function(done) {
                 crypto.randomBytes(20, function(err, buf) {
                     var token = buf.toString('hex');
+                    log(`token created: ${token}`)
                     done(err, token);
                 });
             },
@@ -129,22 +134,21 @@ module.exports = {
                 User.findOne({
                     email: req.body.email
                 }, function(err, user) {
-                    if(err) console.error(err)
-                    console.log('email', req.body.email)
+                    if(err) error(err)
+                    log('email: ', req.body.email)
                     if (!user) {
-                        console.log('No user with that email')
+                        error('No user with that email')
                         req.flash('error', 'No account with that email address exists.');
-                        return res.redirect('/forgot');
+                        return res.redirect('forgot');
                     }
                     user.resetPasswordToken = token;
                     user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
 
                     user.save(function(err) {
-                        if (err)
-                            console.error('An error occured in saving')
+                        if (err) error('An error occured in saving')
                         done(err, token, user);
-                        console.log('user token saved')
-                        console.log('USER', user)
+                        log('user token saved')
+                        log('USER', user)
                     });
                 });
             },
@@ -163,17 +167,18 @@ module.exports = {
                     text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' + 'Please click on the following link, or paste this into your browser to complete the process:\n\n' + 'http://' + req.headers.host + '/reset/' + token + '\n\n' + 'If you did not request this, please ignore this email and your password will remain unchanged.\n'
                 };
                 smtpTransport.sendMail(mailOptions, function(err) {
-                    console.log('mail sent');
+                    log('mail sent');
                     req.flash('success', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
                     done(err, 'done');
                 });
             }
         ], function(err) {
             if (err)
-                return next(err);
-            res.redirect('/forgot');
+            next(err);
+            return res.redirect('forgot');
         });
     },
+    // display user reset form
     pwTokenGet: (req, res) => {
         User.findOne({
             resetPasswordToken: req.params.token,
@@ -181,10 +186,11 @@ module.exports = {
                 $gt: Date.now()
             }
         }, function(err, user) {
-            console.log('USER', user)
+            log('USER', user)
             if (!user) {
+                error('Password reset token is invalid or has expired. Or a networking error occured.')
                 req.flash('error', 'Password reset token is invalid or has expired. Or a networking error occured.');
-                return res.redirect('/forgot');
+                return res.redirect('forgot');
             }
             res.render('reset', {
                 method: 'POST',
@@ -207,11 +213,12 @@ module.exports = {
                 field_two_placeholder: 're-enter password',
                 button_type: 'submit',
                 button_value: 'submit',
-                buttonField: "Reset"
+                buttonField: 'Reset'
 
             });
         });
     },
+    // post user PW reset
     pwTokenPost: (req, res) => {
         async.waterfall([
             function(done) {
@@ -222,38 +229,40 @@ module.exports = {
                     }
                 }, function(err, user) {
                     if (!user) {
+                        error('Password reset token is invalid or has expired.')
                         req.flash('error', 'Password reset token is invalid or has expired.');
                         return res.redirect('back');
                     }
                     if (req.body.password !== req.body['password-confirmation']) {
-                        req.flash("error", "Passwords do not match.");
+                        req.flash('error', 'Passwords do not match.');
                         return res.redirect('back');
                     }
                     if(!module.exports.passwordVerify){
+                        error('Password is too short. Must be at least eight characters.')
                         req.flash('info', 'Password is too short. Must be at least eight characters.')
+                        return res.redirect('back');
                     }
                     // create hash from pw
                     bcrypt.hash(req.body.password, saltRounds).then(function(hash) {
-                        console.log('hash', hash)
+                        log('hash', hash)
                         // save new hash
                         user.password = hash
                         // empty reset data
                         user.resetPasswordToken = null
                         user.resetPasswordExpires = null
-                        //TODO- add PW checking function
                         let promise = user.save()
                         promise.then(userData => {
-                            console.log('user saved')
+                            log('user saved')
                             req.flash('success', 'Password changed.')
                             res.redirect('/')
                         }).catch(err => {
-                            console.log('an error occured', err)
+                            error('an error occured', err)
                             req.flash('error', `An Error occurred in saving: ${err}`)
-                            res.redirect('/forgot')
+                            res.redirect('forgot')
                         })
 
                     }).catch(err => {
-                        console.log("There was an err in hashing", err)
+                        error('There was an err in hashing', err)
                     })
                 });
             },
@@ -272,27 +281,28 @@ module.exports = {
                     text: 'Hello,\n\n' + 'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
                 };
                 smtpTransport.sendMail(mailOptions, function(err) {
+                    log('Password changed')
                     req.flash('success', 'Success! Your password has been changed.');
                     done(err);
                 });
             }
         ], function(err) {
-            console.error('a waterfall error occured');
-            res.redirect('/forgot');
+            error('a waterfall error occured');
+            res.flash('error', 'And error occured during reset.')
+            res.redirect('forgot');
         });
     },
+    // check password length
     passwordVerify: (pw) => {
-
         if(typeof pw !== 'string'){
-            console.error('error: password must be a string')
+            error('verify: fire must be a string block')
             return false
         }
         if(pw.length < 8){
-            console.log('Password too short. Must be 8 characters.')
+            error('verify: fire too short block')
             return false
         }
         return true
-
     },
     // forgot password view page
     forgotPasswordView: (req, res) => {
@@ -310,7 +320,7 @@ module.exports = {
             field_one_placeholder: 'Enter email',
             button_type: 'submit',
             button_value: 'submit',
-            buttonField: "Reset"
+            buttonField: 'Reset'
         })
     }
 }
