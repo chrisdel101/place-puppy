2c7af60cd0f2520dfa481e9b20a8db4748dde655
Chris Del <drumgod101@gmail.com>
2020-08-06 14:30:51 -0600

    remove junk from index route

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const Image = require('../logic/models/image.model')
 const imageController = require('../logic/controllers/images.controller')
 
 const indexController = require('../logic/controllers/index.controller')
 const imageMiddleware = require('../logic/middleware/images.middleware.js')
-
-const cloudinary = require('cloudinary')
-let publicImageId = ''
-const fs = require('fs')
-const {
-  sessionCheck,
-  fullSeed,
-  displayCloud,
-  cloudinaryUploader,
-  singleSeed,
-} = require('../logic/utils')
-const debug = require('debug')
-const log = debug('app:log')
-const error = debug('app:error')
-const session = require('express-session')
 
 // show index
 router.get('/', indexController.showIndex)
@@ -29,8 +13,6 @@ router.get(
   imageMiddleware.qualityMiddleware,
   imageMiddleware.returnImageFormat,
   (req, res) => {
-    console.log('HERE ')
-    console.log('format', req.format)
     imageController.showImage(req, res, req.quality, req.format)
   }
 )
